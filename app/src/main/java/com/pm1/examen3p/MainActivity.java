923c45bc251cb03be82d38d77531f4595b43239e
Joshua Mclean <86990609+JoshuaMc1@users.noreply.github.com>
2022-08-18 21:11:14 -0600

    Initial commit

---
+++ app/src/main/java/com/pm1/examen3p/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pm1.examen3p;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
